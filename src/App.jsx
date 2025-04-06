d935779a2569faccc958476f9b2a17d4c1b2d835
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-04-06 13:51:03 +0600

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-        
+      <div className='w-11/12 mx-auto'> 
+        <Navbar></Navbar>   
+      </div> 
     </>
   )
 }
